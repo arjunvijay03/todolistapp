19ca401b1a71310d854cd0518a8f8f84ba836e0d
Arjun Vijay <arjunvijay2002113@gmail.com>
2023-05-28 18:48:24 +0530

    add github workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://arjunvijay03.github.io/todolistapp",
+  base:"/todolistapp/",
   plugins: [react()],
   server:{
     port:3000
