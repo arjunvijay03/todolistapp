5a9e0a25f5b22469eb25cc2bb06e4a8fe21929f5
Arjun Vijay <arjunvijay2002113@gmail.com>
2023-05-28 18:41:05 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://arjunvijay03.github.io/todolistapp",
   plugins: [react()],
   server:{
     port:3000
